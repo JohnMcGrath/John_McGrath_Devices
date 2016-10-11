574bb23100fb3039684f086ac79aa95747cc4055
Johnny McGrath <Johnny McGrath>
2016-10-11 10:55:59 +0100

    Lab 4-3 Near Done

--- Lab4/RefactoredSquares.js
+++ Lab4/RefactoredSquares.js
@@ -1,13 +1,20 @@
 console.log("RefactoredSquares.js Loaded");
-
-var xPos = 100;
-var yPos = 100;
-
 app={};
 
 function main()
 {
+    app.xPos = 100;
+    app.yPos = 100;
 
+    app.ctx;
+
+    //create the document
+    app.canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+
+    //set the canvas size
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
 	console.log("Main Called in RefactoredSquares.js");
 	console.log("Hello world");
@@ -15,32 +22,32 @@ function main()
 //do the random fuction
 	Math.random();
 
-	var s = new Square(xPos,yPos,rgb(0, 150, 150));
+	app.s = new Square(app.xPos,app.yPos,rgb(0, 150, 150));
 	
 }
 
 
 document.addEventListener('keydown', function(event) {
- ctx.clearRect(0,0,canvas.width, canvas.height);
+ app.ctx.clearRect(0,0,canvas.width, canvas.height);
 
     if(event.keyCode == 37) {
         console.log('Left was pressed');
-        xPos -= 50;
+        app.xPos -= 50;
     }
     else if(event.keyCode == 38) {
          console.log('Up was pressed');
-         yPos -= 50;
+         app.yPos -= 50;
     }
     if(event.keyCode == 39) {
          console.log('Right was pressed');
-         xPos += 50;
+         app.xPos += 50;
     }
     else if(event.keyCode == 40) {
          console.log('Down was pressed');
-         yPos += 50;
+         app.yPos += 50;
     }
 
-    var s = new Square(xPos,yPos,rgb(0, 150, 150));
+    app.s = new Square(xPos,yPos,rgb(0, 150, 150));
 });
 
 
--- Lab4/Square.js
+++ Lab4/Square.js
@@ -4,30 +4,14 @@
 //Alerts are used to quickly tell what was loaded/used
 console.log("Squares.js Loaded");
 
-	var ctx;
-	var canvas;
-
-	//create the document
-	canvas = document.createElement("canvas");
-	document.body.appendChild(canvas);
-
-	//set the canvas size
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-
-
-
 function Square(x,y,colour){
-
-this.m_x = x;
-this.m_y = y;
-this.m_colour = colour;
+	this.m_x = x;
+	this.m_y = y;
+	this.m_colour = colour;
 
 	console.log("Square Function in Square.js Called");
-		ctx = canvas.getContext("2d");
-		ctx.fillStyle = this.m_colour;
-
-		ctx.fillRect(this.m_x,this.m_y,100,100);
-		//ctx.fillRect(this.m_x,this.m_y,Math.random() * 100,Math.random() *100);
-		ctx.stroke();
+	app.ctx = canvas.getContext("2d");
+	app.ctx.fillStyle = this.m_colour;
+	app.ctx.fillRect(this.m_x,this.m_y,100,100);
+	app.ctx.stroke();
 }
