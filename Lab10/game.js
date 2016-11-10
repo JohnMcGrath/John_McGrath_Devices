e0eb328126a4714a0f8107def02a276cad03a802
Johnny McGrath <Johnny McGrath>
2016-11-10 14:37:51 +0000

    Lab 10 - 4 Done

--- Lab10/game.js
+++ Lab10/game.js
@@ -1,3 +1,29 @@
+var fps = 15;
+
+var ctx;
+var canvas;
+
+var sImgX = 0;
+
+var oldTime = Date.now();
+
+//Make sure the image is loaded before attempting to use
+var myImage = new Image();   // Create new img element
+myImage.addEventListener("load", function() {
+  // execute drawImage statements here
+}, false);
+myImage.src = 'walkSprite.png'; // Set source path
+
+//create the document
+canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+//set the canvas size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx = canvas.getContext("2d");
+
 var Game = function(){
 	this.init();
 };
@@ -7,6 +33,19 @@ Game.prototype.init = function(){
 }
 
 Game.prototype.update = function(){
-	console.log("Updated");
+		if (Date.now() - oldTime > 1000 / fps)
+	{
+		if (sImgX == 3)
+		{
+			sImgX = 0;
+		}
+
+		sImgX++;
+
+		oldTime = Date.now();
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		ctx.drawImage(myImage, sImgX*250,0,250,500,0,0,100,200);
+		console.log("ImageDrawn");
+	}
 	window.requestAnimationFrame(myGame.update);
 }
--- Lab10/main.js
+++ Lab10/main.js
@@ -1,31 +1,6 @@
-//Global Variable
-app = {};
-var fps = 30;
-
-var ctx;
-var canvas;
-
-//Make sure the image is loaded before attempting to use
-var myImage = new Image();   // Create new img element
-myImage.addEventListener("load", function() {
-  // execute drawImage statements here
-}, false);
-myImage.src = 'pikachu.png'; // Set source path
-
-//create the document
-canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-
-//set the canvas size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-ctx = canvas.getContext("2d");
 
 function main()
 {
 	myGame = new Game();
-	myGame.update();
-
-	ctx.drawImage(myImage, 10,10);
+	myGame.update();	
 }
