f147ade110c095076cfac57ecc7b478d3d254dc9
Johnny McGrath <Johnny McGrath>
2016-11-10 13:59:16 +0000

    Lab 10 - 2 Complete

--- Lab10/main.js
+++ Lab10/main.js
@@ -1,9 +1,16 @@
 //Global Variable
-var App = {};
+app = {};
 var fps = 30;
 
 var ctx;
 var canvas;
+
+//Make sure the image is loaded before attempting to use
+var myImage = new Image();   // Create new img element
+myImage.addEventListener("load", function() {
+  // execute drawImage statements here
+}, false);
+myImage.src = 'pikachu.png'; // Set source path
 
 //create the document
 canvas = document.createElement("canvas");
@@ -13,8 +20,12 @@ document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+ctx = canvas.getContext("2d");
+
 function main()
 {
 	myGame = new Game();
 	myGame.update();
+
+	ctx.drawImage(myImage, 10,10);
 }
