1acb3ec89f3925eaf2785fbf19bd1386adb61dbf
Johnny McGrath <Johnny McGrath>
2016-11-10 14:47:57 +0000

    Lab 10 - 5

--- Lab10/game.js
+++ Lab10/game.js
@@ -4,8 +4,10 @@ var ctx;
 var canvas;
 
 var sImgX = 0;
+var otherSImgX = 0;
 
 var oldTime = Date.now();
+var otherOldTime = Date.now();
 
 //Make sure the image is loaded before attempting to use
 var myImage = new Image();   // Create new img element
@@ -13,6 +15,13 @@ myImage.addEventListener("load", function() {
   // execute drawImage statements here
 }, false);
 myImage.src = 'walkSprite.png'; // Set source path
+
+//Make sure the image is loaded before attempting to use
+var myOtherImage = new Image();   // Create new img element
+myOtherImage.addEventListener("load", function() {
+  // execute drawImage statements here
+}, false);
+myOtherImage.src = 'walkSprite.png'; // Set source path
 
 //create the document
 canvas = document.createElement("canvas");
@@ -33,7 +42,7 @@ Game.prototype.init = function(){
 }
 
 Game.prototype.update = function(){
-		if (Date.now() - oldTime > 1000 / fps)
+	if (Date.now() - oldTime > 1000 / fps)
 	{
 		if (sImgX == 3)
 		{
@@ -43,9 +52,25 @@ Game.prototype.update = function(){
 		sImgX++;
 
 		oldTime = Date.now();
-		ctx.clearRect(0,0,canvas.width,canvas.height);
 		ctx.drawImage(myImage, sImgX*250,0,250,500,0,0,100,200);
 		console.log("ImageDrawn");
 	}
+
+
+	if (Date.now() - otherOldTime > 1000 / 50)
+	{
+		if (otherSImgX == 3)
+		{
+			otherSImgX = 0;
+		}
+
+		otherSImgX++;
+
+		otherOldTime = Date.now();
+		ctx.drawImage(myOtherImage, otherSImgX*250,0,250,500,200,0,100,200);
+		console.log("ImageDrawn");
+	}
+
+
 	window.requestAnimationFrame(myGame.update);
 }
