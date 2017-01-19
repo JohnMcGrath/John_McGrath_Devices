4fca3cb28882fa3e23860e687c91c7d133558ace
Johnny McGrath <Johnny McGrath>
2017-01-19 13:29:11 +0000

    Lab 11-1 Done

---
+++ Lab 11/server.py
@@ -0,0 +1,28 @@
+from tornado import websocket, web, ioloop, httpserver
+import tornado
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+
+	def check_origin(self, origin):
+		return True
+		
+	def open(self):
+		print("Connection Opened")
+		#pass
+
+	def on_message(self, message):
+		print("You said: ")
+		#pass			
+ 
+	def on_close(self):
+	    pass
+
+app= tornado.web.Application([
+	#map the handler to the URI named "test"
+	(r'/wstest', WSHandler),
+])
+
+if __name__ == '__main__':
+	server_port=8080
+	app.listen(server_port)
+	ioloop.IOLoop.instance().start()
