518120b8a9b7152130acf53e1505bf2bd72a47d2
Johnny McGrath <Johnny McGrath>
2017-02-20 17:29:14 +0000

    Networking Updated

--- Lab 13/game.js
+++ Lab 13/game.js
@@ -3,54 +3,42 @@ var ws = new WebSocket("ws://localhost:8080/wstest");
 updateState = function(){
 	var cursorX = event.clientX;     // Get the horizontal coordinate
 	var cursorY = event.clientY;     // Get the vertical coordinate
-	console.log(cursorX);
-	console.log(cursorY);
 	var updateState = {};
 	updateState.type = "updateState"
 	updateState.data = {x:cursorX,  y:cursorY};
 
 	var  updateMessage = JSON.stringify(updateState);
-	console.log(updateMessage);
 	ws.send(updateMessage);
 };
 
 myJoin = function(){
 	var message = {}
 	message.type = "join";
-	message.date = "";
+	message.data = "";
 	var joinMessage = JSON.stringify(message);
 	ws.send(joinMessage);
 };
 
-
 document.onclick = function(){
 	var joinButton = document.getElementById("join");
 	joinButton.addEventListener("click", join);
-	myJoin();
 	updateState();
+};
+
+document.getElementById("join").onclick = function(){
+	myJoin();
 }
 
 ws.onopen = function() {
-    var objMessage = {};
-	objMessage.type = "test";
-	objMessage.data = "hello";
-
-	
-
-	var jsonMessage = JSON.stringify(objMessage);
-	ws.send(jsonMessage);
-	updateState();
 };
 
 
 //called when the client receives a message
 ws.onmessage = function(evt) {
 	if (evt.type == "updateState"){
-		console.log(evt.data);
 		var jsonObject = JSON.parse(evt.data);
-		console.log(jsonObject);
 	}
 	if (evt.type == "join"){
-		console.log("JOINING PLAYER");
+		var jsonObject = JSON.parse(evt.data);
 	}
 };
--- Lab 13/server.py
+++ Lab 13/server.py
@@ -7,6 +7,7 @@ session = {}
 
 WAITING_FOR_PLAYERS = 0
 GAME_IN_PROGRESS = 1
+game_state = WAITING_FOR_PLAYERS
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 
@@ -19,24 +20,20 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 		print("Player Address: " + player_address)
 		connection[player_address] = self
 
-	def on_message(self, message):
-		self.write_message(message)
-		self.send_to_other_player(message)
-		print('********************', message)
-
-		messageDict = json.loads(message)
-		print(messageDict)
-
-		if messageDict["type"] == "join":
-			print("PLAYER 2 HAS JOINED")
-
-		if messageDict["type"] == "updateState":
-			self.send_to_other_player(messageDict)
-
 	def send_to_other_player(self, message):
 		for key, value in connection.items():
 			if (key != self.get_player_address()):
 				value.write_message(message)
+
+	def join(self):
+		print("Join Function Started")
+		if len(session)<2:
+			player_address =self.get_player_address()
+			session[player_address] = self
+
+		if len(session)==2:
+			print("Full Game")
+			game_state=GAME_IN_PROGRESS
 
 	def on_close(self):
 		pass
@@ -44,7 +41,16 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 	def get_player_address(self):
 		return self.request.remote_ip+ ", " + str(self.stream.socket.getpeername()[1])
 
+	def on_message(self, message):
+		messageDict = json.loads(message)
 
+		if messageDict["type"] == "join":
+			self.join()
+			print("Joining in on_method")
+
+		if messageDict["type"] == "updateState":
+			self.send_to_other_player(messageDict)
+			print("Updating in on_method")
 
 
 app= tornado.web.Application([
